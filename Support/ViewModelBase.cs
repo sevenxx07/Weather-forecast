cd2c1ccfad22e22b02385aba61b1046f2b25cc3f
Sára Veselá <104825252+sevenxx07@users.noreply.github.com>
2024-08-09 17:17:56 +0200

    Add files via upload

---
+++ Support/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUR_Task3_assignment.Support;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    protected ViewModelBase()
+    {
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        if (handler != null)
+        {
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+    }
+
+    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+}
